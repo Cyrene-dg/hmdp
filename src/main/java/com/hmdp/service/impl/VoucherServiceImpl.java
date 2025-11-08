0f674e013b0fbb5d4d4c132f8ecb427f986dcb91
Cyrene <m16603557170@163.com>
2025-11-08 15:53:16 +0800

    秒杀优化的业务逻辑，并实现将新增秒杀券存入redis的功能

--- src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
@@ -7,6 +7,11 @@ import com.hmdp.mapper.VoucherMapper;
 import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherService;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +31,9 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
 
     @Resource
     private ISeckillVoucherService seckillVoucherService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
 
     @Override
     public Result queryVoucherOfShop(Long shopId) {
@@ -35,6 +43,12 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
         return Result.ok(vouchers);
     }
 
+    //由于优惠券秒杀业务中对数据库的依赖过强，导致性能不好，故将库存判断（商品乐观锁）和一人一单（用户悲观锁）业务转移到redis
+    //具体实现逻辑
+    //新增秒杀券的同时将秒杀券的信息储存到redis
+    //使用lua脚本来实现库存判断和一人一单的redis业务
+    //将抢购成功后的用户id和优惠券id存入阻塞队列
+    //后续载将阻塞队列中的新信息同步到数据库
     @Override
     @Transactional
     public void addSeckillVoucher(Voucher voucher) {
@@ -47,5 +61,9 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
         seckillVoucher.setBeginTime(voucher.getBeginTime());
         seckillVoucher.setEndTime(voucher.getEndTime());
         seckillVoucherService.save(seckillVoucher);
+
+        //将秒杀券信息存入redis，key是秒杀券id，值是秒杀券库存
+      stringRedisTemplate.opsForValue().set(RedisConstants.SECKILL_STOCK_KEY+voucher.getId(), voucher.getStock()+"");
+
     }
 }
