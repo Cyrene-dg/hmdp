c074350eb1121464d53c91b7f845698cb9d6b014
Cyrene <m16603557170@163.com>
2025-11-09 15:28:17 +0800

    blog查询接口

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,9 +1,12 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +20,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+    @Override
+    public Result getBlogById(Long id) {
+        //从数据库里拿blog，已知blogid，需要拿到其他blog信息以及用户icon和用户name
+        //这里的用户icon和用户name的设计很巧妙
+        Blog blog = getById(id);
+        if (blog == null) {
+            return Result.fail("blog不存在");
+        }
+        Long userId = blog.getUserId();
+        User user = userServiceImpl.getById(userId);
+        blog.setIcon(user.getIcon());
+        blog.setName(user.getNickName());
+        return Result.ok(blog);
+    }
 }
